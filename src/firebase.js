e77edb96442ec8c3a7d0394dddea7c4a3d1d4f4b
exZon exZon <yashpatel5566789>
2020-08-27 19:49:14 +0530

    final complete

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDM6qtyhezIcTo95glsBAQ17bnwQAdN3y8",
+    authDomain: "airbnb-clone-3f8c4.firebaseapp.com",
+    databaseURL: "https://airbnb-clone-3f8c4.firebaseio.com",
+    projectId: "airbnb-clone-3f8c4",
+    storageBucket: "airbnb-clone-3f8c4.appspot.com",
+    messagingSenderId: "419954606375",
+    appId: "1:419954606375:web:7d8a7fcaf3a20c5c462902",
+    measurementId: "G-GBRHY7020H"
+  };
